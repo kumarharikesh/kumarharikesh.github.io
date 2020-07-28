c5997e4202b84329bea43bc3b1ee9ecd3db345bc
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2020-07-28 23:06:24 +0530

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -51,3 +51,35 @@ model.addEventListener("click", function(e) {
     panel.style.width = "0";
   }
 });
+
+//==================================pop Up Windows============================================
+
+function new_Window() {
+//   if (this.innerHTML === "Model") {
+//     window.open("http://127.0.0.1:5500/PROJECT3/Model.html");
+//   }
+  if (this.innerHTML === "Launch Terminal →") {
+    window.open(
+      "https://lightsail.aws.amazon.com/ls/remote/ap-south-1/instances/Harry_Amazon_Linux-1/terminal?protocol=ssh",
+      "_blank",
+      "height=800,width=1000,top=30,left=30"
+    );
+  }
+//   if (this.innerHTML === "Calculator") {
+//     window.open(
+//       "http://127.0.0.1:5500/PROJECT1/Calculator.html",
+//       "_blank",
+//       "height=400,width=600,top=72,left=100"
+//     );
+//   }
+//   if (this.innerHTML === "Pop-up Window") {
+//     var New_Window = window.open(
+//       "",
+//       "_blank",
+//       "height=400, width=600, top=72, left=100"
+//     );
+//     New_Window.document.write("Welcome to new Window !!");
+//     // New_Window.document.close();
+//     // New_Window.close();
+//   }
+}
